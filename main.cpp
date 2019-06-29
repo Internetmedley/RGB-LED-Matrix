c30ee80a5442a03d2bb00c327da1d7859aa02cef
internetmedley <remy.db@live.nl>
2019-06-29 16:04:51 +0200

    outline voor tetris

--- main.cpp
+++ main.cpp
@@ -34,40 +34,17 @@ int main(){
     matrix::Buffer buf( lat, oe, clk, rows, rgb1, rgb2 );
     
     //buf.write_rand_col( hwlib::xy(15, 16) );
-    matrix::EmptyRectangle rect1( hwlib::xy(2, 15), hwlib::xy(14, 20), CYAN );
-    matrix::Rectangle rect2( hwlib::xy(13, 8), hwlib::xy(18, 17), PURPLE );
-    matrix::Line line1( hwlib::xy(5, 2), hwlib::xy(60, 30), YELLOW );
-    matrix::Circle circle1( hwlib::xy( 40, 16), 3, PURPLE );
+    //matrix::EmptyRectangle rect1( hwlib::xy(2, 15), hwlib::xy(14, 20), CYAN );
+    //matrix::Rectangle rect2( hwlib::xy(13, 8), hwlib::xy(18, 17), PURPLE );
+    //matrix::Line line1( hwlib::xy(5, 2), hwlib::xy(60, 30), YELLOW );
+    //matrix::Circle circle1( hwlib::xy( 40, 16), 3, PURPLE );
+    matrix::EmptyRectangle outline1( hwlib::xy(4, 10), hwlib::xy(27, 55), WHITE );                                                                                                           
     
-    rect1.draw( buf );
-    rect2.draw( buf );
-    line1.draw( buf );
-    circle1.draw( buf );
-    
-    /*buf.write( hwlib::xy(0, 0), PURPLE );
-    buf.write( hwlib::xy(1, 0), PURPLE );
-    buf.write( hwlib::xy(2, 0), RED );
-    buf.write( hwlib::xy(3, 0), PURPLE );
-    buf.write( hwlib::xy(4, 0), PURPLE );
-    buf.write( hwlib::xy(5, 0), GREEN );
-    buf.write( hwlib::xy(7, 0), PURPLE );
-    buf.write( hwlib::xy(6, 0), PURPLE );
-    buf.write( hwlib::xy(0, 1), PURPLE );
-    buf.write( hwlib::xy(1, 1), PURPLE );
-    buf.write( hwlib::xy(2, 1), BLUE );
-    buf.write( hwlib::xy(3, 1), PURPLE );
-    buf.write( hwlib::xy(4, 1), PURPLE );
-    buf.write( hwlib::xy(5, 1), PURPLE );
-    buf.write( hwlib::xy(6, 1), WHITE );
-    buf.write( hwlib::xy(7, 1), PURPLE );*/
-    
-    
-    //buf.dataport[15][0] = 0x77;
-    //buf.dataport[14][0] = 0x77;
-    //buf.dataport[13][0] = 0x77;
-    //buf.dataport[12][0] = 0x77;
-    //buf.dataport[11][0] = 0x77;
-    //buf.dataport[0][0] = 0x77
+    //rect1.draw( buf );
+    //rect2.draw( buf );
+    //line1.draw( buf );
+    //circle1.draw( buf );
+    outline1.draw( buf );
     
     
     for( ;; ){
