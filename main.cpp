ae0fd4a47d91c16d7693cdcf661fea69de84a90c
internetmedley <remy.db@live.nl>
2019-07-01 22:22:54 +0200

    tetromino hpp test

--- main.cpp
+++ main.cpp
@@ -35,12 +35,20 @@ int main(){
     matrix::Buffer buf( lat, oe, clk, rows, rgb1, rgb2 );
     
     //buf.write_rand_col( hwlib::xy(15, 16) );
-    //matrix::EmptyRectangle rect1( hwlib::xy(2, 15), hwlib::xy(14, 20), CYAN );
-    //matrix::Rectangle rect2( hwlib::xy(13, 8), hwlib::xy(18, 17), PURPLE );
-    //matrix::Line line1( hwlib::xy(5, 2), hwlib::xy(60, 30), YELLOW );
-    //matrix::Circle circle1( hwlib::xy( 40, 16), 3, PURPLE );
+    matrix::EmptyRectangle rect1( hwlib::xy(2, 15), hwlib::xy(14, 20), CYAN );
+    matrix::Rectangle rect2( hwlib::xy(13, 8), hwlib::xy(18, 17), PURPLE );
+    matrix::Line line1( hwlib::xy(5, 2), hwlib::xy(60, 30), YELLOW );
+    matrix::Circle circle1( hwlib::xy( 40, 16), 3, PURPLE );
     matrix::EmptyRectangle outline1( hwlib::xy(11, 5), hwlib::xy(52, 26), WHITE );    
     matrix::EmptyRectangle outline2( hwlib::xy(10, 4), hwlib::xy(53, 27), WHITE ); 
+    tetris::I_shape I( hwlib::xy(12, 6) );
+    tetris::O_shape O( hwlib::xy(31, 20) );
+    tetris::T_shape T( hwlib::xy(20, 12) );
+    tetris::Z_shape Z( hwlib::xy(13, 6) );
+    tetris::S_shape S( hwlib::xy(20, 10) );
+    tetris::L_shape L( hwlib::xy(12, 6) );
+    tetris::J_shape J( hwlib::xy(48, 22) );
+    
     
     
     //rect2.draw( buf );
@@ -48,8 +56,13 @@ int main(){
     //circle1.draw( buf );
     outline1.draw( buf );
     outline2.draw( buf );
-    tetris::I_shape I;
     I.draw( buf );
+    O.draw( buf );
+    T.draw( buf );
+    Z.draw( buf );
+    S.draw( buf );
+    L.draw( buf );
+    J.draw( buf );
     
     for( ;; ){
         buf.sketch();
